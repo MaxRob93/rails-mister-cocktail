72299354b4226de6a696865a079eb952cfa5ed76
MaximeRoberti <maxime.roberti@gmail.com>
2018-03-16 17:26:12 +0100

    Adding ingredients with json

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.destroy_all
-p "Destroying all data"
-p "Creating freshly new added ingredients"
-Ingredient.create!(name: "lemon")
-Ingredient.create!(name: "ice cubes")
-Ingredient.create!(name: "mint leaves")
-Ingredient.create!(name: "lime")
-Ingredient.create!(name: "white sugar")
-Ingredient.create!(name: "white rum")
-Ingredient.create!(name: "club soda")
-Ingredient.create!(name: "tomato juice")
-Ingredient.create!(name: "vodka")
-Ingredient.create!(name: "Worcestershire sauce")
-Ingredient.create!(name: "freshly grated horseradish")
-Ingredient.create!(name: "hot pepper sauce (Tabasco)")
-Ingredient.create!(name: "salt")
-Ingredient.create!(name: "black pepper")
-Ingredient.create!(name: "lemon juice")
-Ingredient.create!(name: "celery")
-p "We are done"
+p "All previous ingredients were deleted!"
+
+require 'json'
+require 'open-uri'
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+json = JSON.parse(response)
+json["drinks"].each do |j|
+ Ingredient.create([{ name: j['strIngredient1']}])
+end
+
+p "All new ingredients were added! Start making some tasty cocktails."
 
